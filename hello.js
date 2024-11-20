82a17f5211bd8c62035bc4d723fdc2991eec7f5c
Dennis <alfreddennis26.adt90@gmail.com>
2024-11-20 09:42:39 +0100

    Revert "add log to hello.js"
    This reverts commit 6d63c1efaff9664bbd40446d6deea969f06f3835.

--- hello.js
+++ hello.js
@@ -1,5 +1,9 @@
 console.log('Hello Git')
+<<<<<<< HEAD
 console.log('Added new log')
 console.log('Hello Github!')
 console.log('Trying out revert')
 
+=======
+console.log('Added new log')
+>>>>>>> parent of 6d63c1e... add log to hello.js
